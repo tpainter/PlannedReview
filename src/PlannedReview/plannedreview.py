348d502c0afe8ff69f81e771ad52ee3508404d2f
tpainter <travis.painter@gmail.com>
2026-08-09 19:18:50 -0500

    basics for tool calls

--- src/PlannedReview/plannedreview.py
+++ src/PlannedReview/plannedreview.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 from typing import Annotated, Sequence, TypedDict
 
 from dotenv import load_dotenv
@@ -7,22 +8,31 @@ GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 import logging
 # Limit the spam from logging INFO messages
-logging.basicConfig()
-logging.getLogger().setLevel(logging.WARNING)
+#logging.basicConfig()
+#logging.getLogger().setLevel(logging.WARNING)
 
 # LlamaIndex & Docling Imports
 from llama_index.core import VectorStoreIndex, Settings
+from llama_index.core.query_engine import BaseQueryEngine
 
 from llama_index.core.agent import FunctionAgent
+from llama_index.llms.openai import OpenAI
 from llama_index.llms.openai_like import OpenAILike
 from llama_index.embeddings.openai_like import OpenAILikeEmbedding
 from llama_index.llms.google_genai import GoogleGenAI
 from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
+from pydantic_ai.models.openai import OpenAIResponsesModel
+from pydantic_ai.providers.openai import OpenAIProvider
 
 # Pydantic Imports
+from pydantic_ai import Agent, FunctionToolset
+from pydantic_ai.models.google import GoogleModel
+from pydantic_ai.providers.google import GoogleProvider
 from pydantic import BaseModel, Field
-from pydantic_ai import Agent
 from typing import List
+
+from dataclasses import dataclass
+
 
 # plannedreview imports
 from tools import db, file_tools, llm
@@ -36,33 +46,30 @@ def setup_llms():
     
     
     # Configure LlamaIndex to use Google Gemini
-    
+    '''
     Settings.llm = GoogleGenAI(
         model="gemini-3.6-flash",
         api_key=GEMINI_API_KEY,
     )
-    # TODO: local model calls have an error when used.
+
     '''
     Settings.llm = OpenAILike(
-        model_name="qwen3.6-27B",
+        model="gemma4-26b",
         api_base="http://192.168.0.50:8080/v1",
         api_key="fake",
         is_function_calling_mode=True,
         is_chat_model=True,
-        timeout=60.0,
+        timeout=600.0,
     )
-    '''
+    
     
     Settings.embed_model = OpenAILikeEmbedding(
         model_name="gemma-embedding",
         api_base="http://192.168.0.50:8080/v1",
-        #api_base="http://localhost:8080/v1",
         api_key="fake",
         embed_batch_size=32,
         timeout=60.0,
     )
-
-    
 
 class Output(BaseModel):
     """Output containing the response, page numbers, and confidence."""
@@ -80,32 +87,49 @@ class Output(BaseModel):
         ..., description="Explanation for the confidence score"
     )
 
-
 def main():
     # Provide a pdf document
-    pdf_path = "data/OhioCounty.pdf" 
+    pdf_name = "OhioCounty.pdf"
+    pdf_path = "data/" + pdf_name 
     
     # 0. Split PDF
     pages_dir = file_tools.pdf_split_pages(pdf_path)
     
     # 1. Setup llms
     setup_llms()
-    sllm = Settings.llm.as_structured_llm(output_cls=Output)
 
-    index = db.vectorStore(pages_dir)
+    index = db.vectorStore(pages_dir, pdf_name)
+
+    @dataclass
+    class AgentDeps:
+        query_engine: BaseQueryEngine
+
+
+    llm_location = 'local'
+    if llm_location == 'google':
+        provider = GoogleProvider(api_key=GEMINI_API_KEY)
+        model = GoogleModel('gemini-3.6-flash', provider=provider)
+    if llm_location == 'local':
+        provider = OpenAIProvider(base_url = "http://192.168.0.50:8080/v1")
+        model = OpenAIResponsesModel(model_name = "gemma4-26b", provider=provider)
+    agent = Agent(model,
+                  deps_type=AgentDeps,
+                  tools=[db.rag_db],
+            )
     
     # 2. Setup Query Engine
     query_engine = index.as_query_engine(
         similarity_top_k=5,
-        llm=sllm,
         response_mode="tree_summarize",  # you can also select other modes like `compact`, `refine`
     )
 
     # 3. Run the query
-    user_query = "What is file name of the sheet where the general notes are located?"
-
-    response = query_engine.query(user_query)
-    print(str(response))
+    deps = AgentDeps(query_engine=query_engine)
+                     
+    user_prompt = "What sheet are the general notes found on? What is the file name in the database?"
+    result = agent.run_sync(user_prompt, deps=deps)
+    print("Agent Response:")
+    print(result.output)
 
 if __name__ == "__main__":
     main()
--- src/PlannedReview/tools/db.py
+++ src/PlannedReview/tools/db.py
@@ -4,32 +4,82 @@ from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core import StorageContext
 from llama_index.readers.docling import DoclingReader
 from llama_index.core import SimpleDirectoryReader
+from pydantic_ai import Agent, RunContext
+from pydantic import BaseModel, Field
+from typing import List
 
-def vectorStore(temp_dir_path: str):
+from dataclasses import dataclass
+from llama_index.core.query_engine import BaseQueryEngine
+
+def vectorStore(temp_dir_path: str, pdf_name: str):
     """
     
     """
-    # Initialize the Docling reader and load the document
-    reader = DoclingReader(export_type=DoclingReader.ExportType.JSON)       
-    dir_reader = SimpleDirectoryReader(
-        input_dir=temp_dir_path,
-        file_extractor={".pdf": reader},
-        filename_as_id=True,
-    )
 
-    print(f"Parsing documents with Docling: {temp_dir_path}...")
-    documents = dir_reader.load_data(temp_dir_path)
-
-    
-
-    chroma_client = chromadb.EphemeralClient()
-    chroma_collection = chroma_client.create_collection("test")
+    chroma_client = chromadb.PersistentClient(path='./data/chroma_db')
+    #check if vectors are already in db for pdf
+    try:
+        chroma_collection = chroma_client.get_collection("pdf_name")
+        collection_exists = True
+        print("Found existing collection. Skipping re-embedding...")
+    except:
+        chroma_collection = chroma_client.get_or_create_collection("pdf_name")
+        collection_exists = False
+        print("Existing collection not found.")
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
-    index = VectorStoreIndex.from_documents(
-        documents, storage_context=storage_context, embed_model=Settings.embed_model
-    )
 
-    print("LlamaIndex vector store complete.")
+    if collection_exists:
+        index = VectorStoreIndex.from_vector_store(
+            vector_store, 
+            embed_model=Settings.embed_model,)        
+    else:
+        # Initialize the Docling reader and load the document
+        reader = DoclingReader(export_type=DoclingReader.ExportType.JSON)       
+        dir_reader = SimpleDirectoryReader(
+            input_dir=temp_dir_path,
+            file_extractor={".pdf": reader},
+            filename_as_id=True,
+        )
+
+        print(f"Parsing documents with Docling: {temp_dir_path}...")
+        documents = dir_reader.load_data(temp_dir_path)
+        index = VectorStoreIndex.from_documents(
+            documents, storage_context=storage_context, embed_model=Settings.embed_model
+        )
+
+        print("LlamaIndex vector store complete.")
+
     return index
 
+class Output(BaseModel):
+    """Output containing the response, page numbers, and confidence."""
+
+    response: str = Field(..., description="The answer to the question.")
+    page_numbers: List[int] = Field(
+        ...,
+        description="The page numbers of the sources used to answer this question. Do not include a page number if the context is irrelevant.",
+    )
+    confidence: float = Field(
+        ...,
+        description="Confidence value between 0-1 of the correctness of the result.",
+    )
+    confidence_explanation: str = Field(
+        ..., description="Explanation for the confidence score"
+    )
+
+@dataclass
+class AgentDeps:
+    query_engine: BaseQueryEngine
+
+
+def rag_db(ctx: RunContext[AgentDeps], query: str) -> str:
+    """Database to query for information from the pdf documents.
+
+    Args:
+        query: the query string to send to the database
+    """
+    print(f"Query: {query}")
+    response = ctx.deps.query_engine.query(query)
+    
+    return str(response)
